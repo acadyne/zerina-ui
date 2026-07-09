05b54c85b8c5fc2a179ee4319270d8ead6f7b8cb
fabian-ss <acadyne@gmail.com>
2026-07-09 02:10:41 -0600

    fix(scaffold): remove tab scaffold export cycle

--- src/patterns/scaffold/index.ts
+++ src/patterns/scaffold/index.ts
@@ -1,7 +1,7 @@
 // src/patterns/scaffold/index.ts
 export * from "./TopAppBar";
 export * from "./MobileScaffold";
-export * from "./tab-scaffold/TabScaffold";
+export * from "./TabScaffold";
 export * from "./BackButton";
 export * from "./FloatingActionButton";
 export * from "./ScreenContent";
