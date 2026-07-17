38f248b3e519bfda2963101feb8a0ef70275cd82
fabian-ss <acadyne@gmail.com>
2026-07-16 19:35:15 -0600

    feat(theme): add theme public module entry

---
+++ src/theme/index.ts
@@ -0,0 +1,31 @@
+// src/theme/index.ts
+
+export {
+  UIThemeProvider,
+  useUITheme,
+} from "./react/theme";
+
+export {
+  ThemeSystem,
+} from "./runtime/theme-system";
+
+export {
+  createThemeDefinition,
+} from "./definitions/theme-definition";
+
+export {
+  validateThemeDefinition,
+} from "./validation/theme-validation";
+
+export {
+  BUILT_IN_THEMES,
+} from "./built-in";
+
+export type {
+  ThemeDefinition,
+  ThemeMetadata,
+  ThemeSource,
+  ThemeTokens,
+  ThemeValidationDiagnostic,
+  ThemeValidationResult,
+} from "./contracts/theme.types";
